d21836989dc7cec243b43ab314a6005ae9450510
NetLSS <futureprogrammer@naver.com>
2021-08-27 15:18:46 +0900

    Feat: 원격 서비스 실행시키기 바인딩하기

--- app/src/main/java/com/lilcode/example/remoteboundservice/MainActivity.kt
+++ app/src/main/java/com/lilcode/example/remoteboundservice/MainActivity.kt
@@ -1,11 +1,37 @@
 package com.lilcode.example.remoteboundservice
 
+import android.content.ComponentName
+import android.content.Context
+import android.content.Intent
+import android.content.ServiceConnection
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.IBinder
+import android.os.Messenger
 
 class MainActivity : AppCompatActivity() {
+
+    var myService: Messenger? = null
+    var isBound: Boolean = false
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val intent = Intent(applicationContext, RemoteService::class.java)
+        bindService(intent, myConnection, Context.BIND_AUTO_CREATE)
+    }
+
+    private val myConnection = object : ServiceConnection {
+        override fun onServiceConnected(name: ComponentName?, service: IBinder?) {
+            myService = Messenger(service)
+            isBound = true
+        }
+
+        override fun onServiceDisconnected(name: ComponentName?) {
+            myService = null
+            isBound = false
+        }
+
     }
 }
