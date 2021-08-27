ef250f296d5a301aa02409a83b40cc1bb2af370d
NetLSS <futureprogrammer@naver.com>
2021-08-27 15:10:44 +0900

    Feat: 원격 바운드 서비스 구현

--- app/src/main/java/com/lilcode/example/remoteboundservice/RemoteService.kt
+++ app/src/main/java/com/lilcode/example/remoteboundservice/RemoteService.kt
@@ -2,11 +2,23 @@ package com.lilcode.example.remoteboundservice
 
 import android.app.Service
 import android.content.Intent
-import android.os.IBinder
+import android.os.*
+import android.widget.Toast
 
 class RemoteService : Service() {
 
+    private  val myMessenger = Messenger(IncomingHandler()) // 핸들러 인스턴스 생성 및 이것을 전달하여 메신저 객체 생성
+
+    // 핸들러 구성하기
+    inner class IncomingHandler : Handler(Looper.getMainLooper()) {
+        override fun handleMessage(msg: Message) {
+            val data = msg.data
+            val dataString = data.getString("MyString")
+            Toast.makeText(applicationContext, "$dataString", Toast.LENGTH_SHORT).show()
+        }
+    }
+
     override fun onBind(intent: Intent): IBinder {
-        TODO("Return the communication channel to the service.")
+        return myMessenger.binder // 메신저의 IBinder 객체를 반환
     }
 }
